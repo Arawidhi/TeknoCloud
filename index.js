efed92bb16cde31085375354c10fe98e7f11a445
Iniara 안녕 하세요 ! <92317862+Arawidhi@users.noreply.github.com>
2022-03-03 18:31:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
   const hostname = '0.0.0.0';
 const port  process.env.PORT || 3000;
  
-  const server = http.createServer((req, res) => (
+  const server = http.createServer((req, res) => {
   res.statusCode  200;
             res.setHeader('Content-Type', 'text/plain');
             res.end('Hello World\n');
-  ));
+  });
   
-  server.listen(port, hostname, () => (
+  server.listen(port, hostname, () => {
             console.log('Server running at http://$(hostname):$(port)/`);
-  ));
+  });
